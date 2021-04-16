1e3557d0e91b2f53839851be63723d212ef249b2
TunkenovNikolay <nik0sh@bk.ru>
2021-04-16 16:25:44 +0700

    Fix bags

--- src/main/java/com/tunkenov/usersbase/controller/UserController.java
+++ src/main/java/com/tunkenov/usersbase/controller/UserController.java
@@ -33,26 +33,26 @@ public class UserController {
     }
 
     @PostMapping("/user-create")
-    public String createUser(User user){
+    public String createUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
 
     @GetMapping("user-delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id){
+    public String deleteUser(@PathVariable("id") Long id) {
         userService.deleteById(id);
         return "redirect:/users";
     }
 
     @GetMapping("user-update/{id}")
-    public String updateUserForm(@PathVariable("id") Long id, Model model){
+    public String updateUserForm(@PathVariable("id") Long id, Model model) {
         User user = userService.findById(id);
         model.addAttribute("user", user);
         return "/user-update";
     }
 
     @PostMapping("/user-update")
-    public String updateUser(User user){
+    public String updateUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
